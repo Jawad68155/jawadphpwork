fc58da4f8b0eee5a049d51924d0c76ece995f658
jawad68155 <jawad68155@gmail.com>
2024-10-03 14:08:09 +0500

    aj ka kaam

--- dashmin/viewcategory.php
+++ dashmin/viewcategory.php
@@ -1,47 +1,46 @@
 <?php
 include("components/header.php");
 ?>
-<!-- Blank Start -->
-<div class="container-fluid pt-4 px-4">
-                <div class="row  bg-light rounded mx-0">
+         <!-- Blank Start -->
+         <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-light rounded mx-0">
                     <div class="col-md-12">
-                        <h3 class="py-3 px-3">All Categories</h3>
-                        <table class="table">
+                        <h3 class="my-3 mx-3">All Categories</h3>
+                        <table class="table my-3">
                                 <thead>
                                     <tr>
-                                        <th scope="col">Id</th>
-                                        <th scope="col">Category Name</th>
+                                        <th scope="col">Category Id</th>
+                                        <th scope="col">Categor Name</th>
                                         <th scope="col">Category Image</th>
-                                        <th scope="col" colspan="2">Actoin</th>
+                                        <th scope="col" colspan="2">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php
-                                    $query = $pdo->query("select * from categories");
-                                    $catRows = $query->fetchAll(PDO::FETCH_ASSOC);
-                                    foreach($catRows as $catValues){
-                                        ?>
-                                        
-                                        
-                                        <tr>
-                                        <th scope="row"><?php echo $catValues['catId']?></th>
-                                        <td><?php echo $catValues['catName']?></td>
-                                        <td><img src="<?php echo $catImgAddress.$catValues['catImage']?>" width="80"></td>
-                                        <td><a href="updatecatagory.php?cid=<?php echo $catValues['catId']?>" class="btn btn-outline-primary">Edit</a></td>
-                                        <td><a href="" class="btn btn-outline-danger">Delete</a></td>
-                                    </tr>
-                                        <?php
-                                    }
+                                  <?php
+                                  $query = $pdo ->query("select * from categories");
+                               
+                                  $row = $query->fetchAll(PDO::FETCH_ASSOC);
+                                  foreach($row as $catRows){
                                     ?>
-                                 
+                                  
+                                  <tr>
+                                        <th scope="row"><?php echo $catRows['catId']?></th>
+                                        <td><?php echo $catRows['catName']?></td>
+                                        <td><img src="<?php echo $categoryImageAddress.$catRows['catImage']?>" width="80"></td>
+                                        <td><a href="updatecategory.php?cid=<?php echo $catRows['catId']?>" class="btn btn-success">Edit</a></td>
+                                        <td><a href="?delete=<?php echo $catRows['catId']?>" class="btn btn-danger">Delete</a></td>
+                                    </tr>
+                                  <?php  
+                                  }
+                                  ?>  
                                    
+                                    
                                 </tbody>
                             </table>
                     </div>
                 </div>
             </div>
             <!-- Blank End -->
-
 <?php
 include("components/footer.php");
 ?>
